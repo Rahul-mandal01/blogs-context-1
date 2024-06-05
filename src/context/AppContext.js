f686a6b7dcb2aad9433908e25af55d7b39129cda
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-05 23:28:44 +0530

    Create AppContext.js

---
+++ src/context/AppContext.js
@@ -0,0 +1,70 @@
+import { createContext } from "react";
+import { baseUrl } from "../baseUrl";
+import { useState } from "react";
+
+
+// -------Step1-------------
+const AppContext = createContext();
+
+export function AppContextProvider({children}){
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState("");
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(null);
+
+    // Data Filling Pending
+
+    async function fetchBlogPosts(page=1){
+
+        setLoading(true);
+        let url = `${baseUrl}?pages=${page}`;
+        try{
+
+            const result = await fetch(url);
+            const data = await result.json();
+            console.log(data);
+            setPage(data.page);
+            setPosts(data.posts);
+            setTotalPages(data.totalPages);
+
+        }
+
+        catch(error){
+                
+            console.log("Error in fetching data");
+            setPage(1);
+            setPosts([]);
+            setTotalPages(null);
+        }
+        setLoading(false);
+
+    }
+
+    function handlePageChange(page){
+        setPage(page);
+        fetchBlogPosts(page);
+    }
+
+
+    const value = {
+        posts,
+        setPosts,
+        loading,
+        setLoading,
+        page,
+        setPage,
+        totalPages,
+        setTotalPages,
+        fetchBlogPosts,
+        handlePageChange,
+    };
+
+
+    // -------Step2-------------
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+
+}
+
+export default AppContext;
