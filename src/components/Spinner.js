578e4416c63f32c543ae6fba96baefd79588e57c
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-06 19:35:36 +0530

    Added CSS

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,10 +1,12 @@
-import React from 'react'
+
+import React from 'react';
 
 const Spinner = () => {
   return (
-    <div className='spinner' ></div>
-  )
-}
-
+    <div className="fixed inset-0 flex items-center justify-center ">
+      <div className="font-bold text-3xl ">Loading</div>
+    </div>
+  );
+};
 
 export default Spinner;
