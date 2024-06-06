c370b5bb22c6251b1c310b22f8fb8b52644134bf
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-06 19:34:56 +0530

    Added JS and CSS

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react'
 import Spinner from './Spinner';
-import AppContext from '../context/AppContext';
+import {AppContext} from '../context/AppContext';
 
 const Blogs = () => {
 
     // consume
-    const {posts, loading} = useContext(AppContext);
+    const {loading, posts} = useContext(AppContext);
 
   return (
-    <div>
+    <div className='flex flex-col gap-y-10 my-[88px] '>
       {
         loading ? 
 
@@ -22,7 +22,23 @@ const Blogs = () => {
           </div>) :
 
           (posts.map( (post) => (
-            <p>{post.title}</p>
+
+            <div key={post.id} className='w-11/12 max-w-2xl mx-auto' >
+                <p className='font-bold text-lg' >{post.title}</p>
+                <p className='text-sm my-1'>
+                  By <span className='italic' >{post.author}</span> on <span className='underline font-semibold cursor-pointer' > {post.category} </span>
+                </p>
+                <p className='text-sm'>
+                  Posted On <span> {post.date} </span>
+                </p>
+                <p className='mt-4 mb-2 '> {post.content} </p>
+                <div className='flex flex-wrap gap-x-2 items-center' >
+                  {post.tags.map( (tag, index) => {
+                    return <span  key={index} className='text-blue-700 underline cursor-pointer font-semibold text-xs' >{ `#${tag}` } </span>
+                  } )}
+                </div>
+            </div>
+
           ) ))
         )
       }
