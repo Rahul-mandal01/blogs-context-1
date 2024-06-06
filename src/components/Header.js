00f97c84fa2d14f05db8656a62d1977bc3634a9f
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-06 20:09:39 +0530

    Update Header Name

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='py-4 border-b-2 border-b-gray-300 drop-shadow-md fixed top-0 inset-x-0 bg-white '>
-      <h1 className="text-3xl font-bold  text-center uppercase" > Codehelp Blogs</h1>
+      <h1 className="text-3xl font-bold  text-center uppercase" > TechVerse Blogs</h1>
       {/* <MdOutlineDarkMode /> */}
       {/* <MdDarkMode/> */}
     </div>
