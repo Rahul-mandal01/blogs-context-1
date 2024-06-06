5988eec2b9f5d8daefa5b7226cd75bd6b7835a65
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-06 19:34:32 +0530

    Added CSS

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Blogs from "./components/Blogs";
 import Pagination from "./components/Pagination";
+import { useContext, useEffect } from "react";
+import { AppContext } from "./context/AppContext";
+
+
 
 
 function App() {
+  const {fetchBlogPosts} = useContext(AppContext);
+
+  useEffect(()=>{
+    fetchBlogPosts();
+  },[]); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
-    <div>
+    <div className="p-3">
       <Header/>
       <Blogs/>
       <Pagination/>
