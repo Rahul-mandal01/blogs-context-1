b81988c14a917d557bd377d0e99008200faadd03
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-06 19:35:11 +0530

    Added CSS

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { MdOutlineDarkMode, MdDarkMode  } from "react-icons/md";
+const Header = () => {
+  return (
+    <div className='py-4 border-b-2 border-b-gray-300 drop-shadow-md fixed top-0 inset-x-0 bg-white '>
+      <h1 className="text-3xl font-bold  text-center uppercase" > Codehelp Blogs</h1>
+      {/* <MdOutlineDarkMode /> */}
+      {/* <MdDarkMode/> */}
+    </div>
+  )
+}
+
+export default Header;
